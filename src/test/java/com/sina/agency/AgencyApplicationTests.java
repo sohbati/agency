154fd6e4cf15256f025f2aebd6570539035be46a
r.sohbati <r.sohbati@niuniverse.com>
2022-05-25 18:04:01 +0430

    initial commit

---
+++ src/test/java/com/sina/agency/AgencyApplicationTests.java
@@ -0,0 +1,17 @@
+package com.sina.agency;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.AssertionsKt;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.util.Assert;
+
+@SpringBootTest
+class AgencyApplicationTests {
+
+	@Test
+	void contextLoads() {
+		Assertions.assertTrue(true);
+	}
+
+}
